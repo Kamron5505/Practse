acaaffb05845fa21c2c633206a2e0f6c9f43a994
Kamron5505 <kama03346@gmail.com>
2025-03-10 22:13:25 +0500

    kakam

---
+++ script.js
@@ -0,0 +1,8 @@
+let bgColor = prompt("Enter the bgcolor");
+if (bgColor) {
+    document.body.style.backgroundColor = bgColor;
+}
+let text = prompt("Enter the text color");
+if(text){
+    document.body.style.color = text;
+}
